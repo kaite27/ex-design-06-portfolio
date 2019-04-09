7ae062a740b1215340db157b3b6145fb21d33118
beigenut <beigenut@gmail.com>
2019-04-08 23:16:55 -0400

    Add Contact Section

--- src/index.js
+++ src/index.js
@@ -40,9 +40,89 @@ $('.experiences').waypoint(function () {
   offset: '80%'
 });
 
+$('.contact').waypoint(function () {
+  $('.contact-form__div').addClass('animated fadeInLeft');
+  setTimeout(function(){
+    $('.form__input:first-child').addClass('animated flash');
+  }, 1300);
+}, {
+  offset: '80%'
+});
+
+// Modal Toggle on Project Section
 const btnLearnMore = document.querySelector(".btn-ghost1");
 
 btnLearnMore.addEventListener("click", e => {
-  console.log('its clicked');
   $('#exampleModal').modal("toggle");
+});
+
+// Tab control on Experiences Section 
+const openTab1 = document.querySelector(".nav-item:nth-child(1)");
+const openTab2 = document.querySelector(".nav-item:nth-child(2)");
+const openTab3 = document.querySelector(".nav-item:nth-child(3)");
+
+openTab1.addEventListener("click", e => {
+  openEvent(event, "work");
+});
+
+openTab2.addEventListener("click", e => {
+  openEvent(event, "educational");
+});
+
+openTab3.addEventListener("click", e => {
+  openEvent(event, "participation");
+});
+
+function openEvent(event, eventName) {
+  const tabContent = document.querySelectorAll(".tab-pane");
+  for (let i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+
+  const tabLinks = document.querySelectorAll(".nav-item");
+  for (let i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  }
+
+  document.querySelector(`#${eventName}`).style.display = "block";
+  event.currentTarget.className += ' active'
+  document.querySelector(`#${eventName}`).className = "tab-pane fade show active";
+}
+
+
+// Scroll-moving to links
+$(document).ready(function () {
+  $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      if (
+        location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length ?
+          target :
+          $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          event.preventDefault();
+          $("html, body").animate({
+              scrollTop: target.offset().top
+            },
+            1000,
+            function () {
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) {
+                return false;
+              } else {
+                $target.attr("tabindex", "-1");
+                $target.focus();
+              }
+            }
+          );
+        }
+      }
+    });
 });
